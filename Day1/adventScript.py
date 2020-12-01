a3421b25d0252c8c5c8bd5b0a06aea143047f7b4
Erika <erikad94@hotmail.ca>
2020-12-01 18:07:23 -0500

    task2 day1

--- Day1/adventScript.py
+++ Day1/adventScript.py
@@ -2,10 +2,17 @@ import sys
 f = open("input.txt", "r")
 
 lines = []
+answer1 = ""
 for val in f.readlines():
     lines.append(int(val))
 for a in lines:
     for b in lines:
         if a+b==2020:
-            print("the answer :", a*b) 
-            sys.exit("You have found the answer")
+            answer1 = str(a*b) 
+            break
+for a in lines:
+    for b in lines:
+        for c in lines:
+            if a+b+c==2020:
+                print("the answers are :", answer1, "and", a*b*c) 
+                sys.exit("You have found the answers")
