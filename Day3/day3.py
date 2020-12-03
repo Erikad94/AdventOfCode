a488dcdf82a959fcf91fbb4ce3d24d4535296f3b
Erika <erikad94@hotmail.ca>
2020-12-03 16:53:15 -0500

    Day3

---
+++ Day3/day3.py
@@ -0,0 +1,20 @@
+import sys
+f = open("input.txt", "r")
+lines = f.readlines()
+
+def calculateTrees(right, down):
+    posX = 0
+    posY = 0
+    answer = 0
+    length = len(lines[posY])-1
+    while posY < len(lines):
+        if(lines[posY][posX] == "#"):
+            answer+=1
+        posX = (posX + right) % length
+        posY += down
+    return answer
+
+print(calculateTrees(3, 1))
+print(calculateTrees(1, 1)*calculateTrees(3, 1)*calculateTrees(5, 1)*calculateTrees(7, 1)*calculateTrees(1, 2))
+
+
