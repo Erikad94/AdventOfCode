a854219bf6161ac8c8a92d4e5a29c7ad2cc23bfb
Erika <erikad94@hotmail.ca>
2020-12-07 18:20:28 -0500

    day7 challenge... i hate recurssion

---
+++ Day7/day7.py
@@ -0,0 +1,49 @@
+import sys
+import re
+
+f = open("input.txt", "r")
+containsStartlenght = 9
+reg = r'^([\w]+ [\w]+)'
+
+
+def hasColor(bags, rules, colorToFind):
+    for bag in bags:
+        if (bag[0] == colorToFind):
+            return True
+    for bag in bags:
+        if hasColor(rules[bag[0]], rules, colorToFind):
+            return True
+    return False
+
+def countBags(bag, rules):
+    bagsInside = rules[bag]
+    totalBagsInside = 0
+    for bagInside in bagsInside:
+        totalBagsInside += countBags(bagInside[0], rules) * bagInside[1]
+    return 1 + totalBagsInside
+
+rules = {}
+for line in f.readlines():
+    bagPos = re.search(r"\d", line)
+    if bagPos is not None:
+        for bag in line[bagPos.start():].split(", "):
+            bagColor = " ".join(bag.split(" ")[1:3])
+            parentBag = re.match(reg, line).group()
+            number = int(bag.split(" ")[0])
+            if parentBag in rules:
+                bagList = rules[parentBag]
+                bagList.append((bagColor, number))
+                rules[parentBag] = bagList
+            else:
+                rules[parentBag] = [(bagColor, number)]
+    else:
+        parentBag = re.match(reg, line).group()
+        rules[parentBag] = []
+
+nbrBag = 0
+for bag in rules:
+    if hasColor(rules[bag], rules, "shiny gold"):
+        nbrBag += 1
+
+print(nbrBag)
+print(countBags("shiny gold", rules)-1)
